2f7c87ea35b40c55bd79348915d47579dc81d1d1
Rasmus Falk <rf222fu@student.lnu.se>
2018-02-08 10:31:05 +0100

    Adding Snippet model

---
+++ models/Snippet.js
@@ -0,0 +1,21 @@
+/**
+ * Module for the Snippet model.
+ *
+ * @module models/Snippet.js
+ * @author Rasmus Falk
+ * @version 1.0.0
+ */
+
+'use strict'
+
+const mongoose = require('mongoose')
+
+const snippetSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  body: { type: String, required: true }
+})
+
+const Snippet = mongoose.model('Snippet', snippetSchema)
+
+// Exports
+module.exports = Snippet
