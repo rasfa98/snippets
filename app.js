b14ea76f3bf7f04d5160162148398572f27a9a90
Rasmus Falk <rf222fu@student.lnu.se>
2018-02-09 11:22:07 +0100

    Setting up session

--- app.js
+++ app.js
@@ -3,18 +3,31 @@ const handlebars = require('express-handlebars')
 const path = require('path')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const credentials = require('./config/credentials')
 
 mongoose.connect('mongodb://db/examination2')
 
 const app = express()
 
-// View engine
 app.engine('.hbs', handlebars({
   defaultLayout: 'main',
   extname: '.hbs'
 }))
 
 app.set('view engine', '.hbs')
+
+app.use(session({
+  name: 'snippet-manager',
+  secret: credentials.secret,
+  saveUnitialized: false,
+  resave: false,
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    maxAge: 900
+  }
+}))
 
 app.use(express.static(path.join(__dirname, '/public')))
 
@@ -25,5 +38,6 @@ app.use('/', require('./routes/create'))
 app.use('/', require('./routes/view'))
 app.use('/', require('./routes/delete'))
 app.use('/', require('./routes/edit'))
+app.use('/', require('./routes/signout'))
 
 app.listen(8000, console.log('Server running...'))
---
+++ config/credentials.js
@@ -0,0 +1,14 @@
+/**
+ * Module for the credentials.
+ *
+ * @module config/credentials.js
+ * @author Rasmus Falk
+ * @version 1.0.0
+ */
+
+ 'use strict'
+
+ // Exports
+ module.exports = {
+   secret: 'qwJhs8T28Z3gfVdOp8YcajXuq689cSM4'
+ }
